40d11455a692c8df1574a207583f00e73ca94efd
Vandemberg <vandemberg.silva.lima@gmail.com>
2018-12-10 15:20:22 -0300

    adicionando automações

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const path = require("path");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+
+// Arquivos internos
+const router = require('./routes');
 
 let url = 'mongodb://vandemberg:1234@localhost/lista-produtos';
 
 // Configurações
 mongoose.connect(url);
 
-// Arquivos internos
-const router = require('./routes');
-
+// Configurações do app
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors({credentials: true, origin: true}));
 app.use(router);
+
+// Carrega o front-end
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // Iniciando servidor
 app.listen(port, () => console.log(`Listening on port ${port}`));
